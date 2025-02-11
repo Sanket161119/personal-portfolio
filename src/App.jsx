fb2c5bc5e81741617c8e84184c44cfad2737d2fc
Sanket161119 <anamikauk4@gmail.com>
2025-02-11 15:20:11 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import {  } from 'react'
+import { useState,useEffect } from 'react'
+import './App.css'
 import Navbar from  './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
@@ -8,7 +9,24 @@ import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
 function App() {
-
+  const[isVisible,setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    if(window.scrollY > 300){
+      setIsVisible(true);
+    }
+    else{
+      setIsVisible(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+  const scrollToTop = () => {
+    window.scrollTo(0,0);
+  }
   return (
     <div>
       <Navbar/>
@@ -18,6 +36,15 @@ function App() {
       <MyWork />
       <Contact />
       <Footer />
+      {isVisible && (
+  <button
+    type='button'
+    className='scroll-to-top'
+    onClick={scrollToTop}
+  >
+    <i className="fa-solid fa-chevron-up"></i>
+  </button>
+)}
     </div>
   )
 }
